3553048d2dd874e8947c49c5ebc71c3f610aed12
Alexandre Canijo <alexandrecanijo@gmail.com>
2024-05-09 00:51:30 +0100

    Adding Prettier and fixing EsLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
+import stylistic from '@stylistic/eslint-plugin';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
+  plugins: { stylistic },
+
   rules: {
     'no-console': 'off',
     'node/prefer-global/process': 'off',
-    'curly': 0,
+    'curly': ['error', 'all'],
     'no-irregular-whitespace': 0,
     'vue/html-self-closing': [
       'error',
@@ -18,6 +21,26 @@ export default withNuxt({
         math: 'always',
       },
     ],
+    '@stylistic/indent': ['error', 2],
+    '@stylistic/quotes': ['error', 'single'],
+    '@stylistic/comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+        enums: 'always-multiline',
+        generics: 'always-multiline',
+        tuples: 'always-multiline',
+      },
+    ],
+    '@stylistic/no-irregular-whitespace': 0,
+    '@stylistic/semi': ['error', 'always'],
+    '@stylistic/arrow-parens': ['error', 'always'],
+    '@stylistic/brace-style': ['error', '1tbs'],
+    '@stylistic/quote-props': ['error', 'consistent'],
   },
 
   ignores: [
@@ -29,4 +52,4 @@ export default withNuxt({
     '__snapshots__/**',
     'src/locales/**',
   ],
-},);
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true, },
-  modules: ['@nuxt/eslint',],
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
   eslint: {
     checker: {
       configType: 'flat',
     },
-    config: {
-      stylistic: {
-        'indent': 2,
-        'quotes': 'single',
-        'semi': true,
-        'arrowParens': true,
-        'quoteProps': 'consistent',
-        'commaDangle': 'always',
-        'object-curly-newline': [
-          'error',
-          {
-            ObjectExpression: {
-              multiline: true,
-            },
-            ObjectPattern: {
-              multiline: true,
-            },
-            ImportDeclaration: 'never',
-            ExportDeclaration: {
-              multiline: true,
-            },
-          },
-        ],
-      },
-    },
   },
-},);
+});
